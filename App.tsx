a7f047de8ceaf2ee151c98e20a79a63791c99766
imamrmdn <imamramadhan420@gmail.com>
2023-07-08 08:01:38 +0700

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * 2023
+ * Gallagher Bus App
+ * Universitas Persada Indonesia Y.A.I
+ * NIM: 1644190068
+ * NAMA: Adam Muhammad Galib
+ */
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ color: "red" }}>testing app gallagher bus for thesis</Text>
+      <Text style={{ color: "red" }}>testing app gallagher bus for thesis</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
