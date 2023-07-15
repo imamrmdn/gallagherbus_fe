fcabbeeb162377b0007bd6c169a39b7235a7f1b6
imamrmdn <imamramadhan420@gmail.com>
2023-07-15 20:44:09 +0700

    revision data koridor

--- src/screens/home/home_screen.tsx
+++ src/screens/home/home_screen.tsx
@@ -22,183 +22,60 @@ import {
   RadioButton,
   Button,
 } from "react-native-paper";
-
-const dataJadwalBus = [
-  {
-    key: 1,
-    halte: "Museum Fatahila",
-    date: "13/07/2023",
-    deparature_time_in_koridor: "10:00",
-    arrival_time_in_koridor: "12:00",
-    schedule_koridor: [
-      {
-        index: "a11",
-        bus_queue: "bus1",
-        deparature_time_bus: "10:00",
-        arrival_time_bus: "10:45",
-      },
-      {
-        index: "a12",
-        bus_queue: "bus2",
-        deparature_time_bus: "10:45",
-        arrival_time_bus: "11:25",
-      },
-      {
-        index: "a13",
-        bus_queue: "bus3",
-        deparature_time_bus: "11:25",
-        arrival_time_bus: "12:00",
-      },
-    ],
-  },
-  {
-    key: 2,
-    halte: "Kota",
-    date: "13/07/2023",
-    deparature_time_in_koridor: "12:00",
-    arrival_time_in_koridor: "14:00",
-    schedule_koridor: [
-      {
-        index: "a21",
-        bus_queue: "bus1",
-        deparature_time_bus: "12:00",
-        arrival_time_bus: "12:45",
-      },
-      {
-        index: "a22",
-        bus_queue: "bus2",
-        deparature_time_bus: "12:45",
-        arrival_time_bus: "13:25",
-      },
-      {
-        index: "a23",
-        bus_queue: "bus3",
-        deparature_time_bus: "13:25",
-        arrival_time_bus: "14:00",
-      },
-    ],
-  },
-  {
-    key: 3,
-    halte: "Glodok",
-    date: "13/07/2023",
-    deparature_time_in_koridor: "14:00",
-    arrival_time_in_koridor: "16:00",
-    schedule_koridor: [
-      {
-        index: "a31",
-        bus_queue: "bus1",
-        deparature_time_bus: "14:00",
-        arrival_time_bus: "14:45",
-      },
-      {
-        index: "a32",
-        bus_queue: "bus2",
-        deparature_time_bus: "14:45",
-        arrival_time_bus: "15:25",
-      },
-      {
-        index: "a33",
-        bus_queue: "bus3",
-        deparature_time_bus: "15:25",
-        arrival_time_bus: "16:00",
-      },
-    ],
-  },
-  {
-    key: 4,
-    halte: "Olimo",
-    date: "13/07/2023",
-    deparature_time_in_koridor: "16:00",
-    arrival_time_in_koridor: "18:00",
-    schedule_koridor: [
-      {
-        index: "a31",
-        bus_queue: "bus1",
-        deparature_time_bus: "14:00",
-        arrival_time_bus: "14:45",
-      },
-      {
-        index: "a32",
-        bus_queue: "bus2",
-        deparature_time_bus: "14:45",
-        arrival_time_bus: "15:25",
-      },
-      {
-        index: "a33",
-        bus_queue: "bus3",
-        deparature_time_bus: "15:25",
-        arrival_time_bus: "16:00",
-      },
-    ],
-  },
-  {
-    key: 5,
-    halte: "Mangga besar",
-    date: "13/07/2023",
-    deparature_time_in_koridor: "18:00",
-    arrival_time_in_koridor: "20:00",
-    schedule_koridor: [
-      {
-        index: "a31",
-        bus_queue: "bus1",
-        deparature_time_bus: "14:00",
-        arrival_time_bus: "14:45",
-      },
-      {
-        index: "a32",
-        bus_queue: "bus2",
-        deparature_time_bus: "14:45",
-        arrival_time_bus: "15:25",
-      },
-      {
-        index: "a33",
-        bus_queue: "bus3",
-        deparature_time_bus: "15:25",
-        arrival_time_bus: "16:00",
-      },
-    ],
-  },
-  {
-    key: 6,
-    halte: "Sawah besar",
-    date: "13/07/2023",
-    deparature_time_in_koridor: "14:00",
-    arrival_time_in_koridor: "16:00",
-    schedule_koridor: [
-      {
-        index: "a31",
-        bus_queue: "bus1",
-        deparature_time_bus: "14:00",
-        arrival_time_bus: "14:45",
-      },
-      {
-        index: "a32",
-        bus_queue: "bus2",
-        deparature_time_bus: "14:45",
-        arrival_time_bus: "15:25",
-      },
-      {
-        index: "a33",
-        bus_queue: "bus3",
-        deparature_time_bus: "15:25",
-        arrival_time_bus: "16:00",
-      },
-    ],
-  },
-];
+import { dataJadwalBus } from "../../utils/data/jadwal_bus";
+import { koridor_schedule } from "../../utils/data/example_data";
 
 export function HomeScreen() {
   //
   const [visible, setVisible] = useState<boolean>(false);
   const [detail, setDetail] = useState<boolean>(false);
-  const [value, setValue] = useState("");
+  const [valueKoridor, setValueKoridor] = useState("");
+  const [koridorName, setKoridorName] = useState("");
+  const [scheduleKoridor, setScheduleKoridor] = useState<any>([]);
 
   const showModal = () => setVisible(true);
   const hideModal = () => setVisible(false);
 
   const showDetail = () => setDetail(true);
   const hideDetail = () => setDetail(false);
+
+  //koridor name
+  const koridor_name = koridor_schedule.map((e) => ({
+    key: e.koridor_key,
+    name: e.koridor_name,
+  }));
+
+  const onHandleFilterKoridor = () => {
+    //
+    const koridor_name = koridor_schedule.map((e) => ({
+      key: e.koridor_key,
+      name: e.koridor_name,
+    }));
+
+    console.log("new_data", koridor_name);
+
+    showModal();
+  };
+
+  const onChangeFilterKoridor = (val: string) => {
+    console.log({ val });
+    setValueKoridor(val);
+  };
+
+  const onOkFilterKoridor = () => {
+    console.log("koridor name:", valueKoridor);
+    const new_schedule_koridor = koridor_schedule.filter(
+      (e) => e.koridor_name === valueKoridor
+    );
+
+    console.log(
+      "new jadwal koridor:",
+      new_schedule_koridor[0].koridor_schedule
+    );
+    setKoridorName(valueKoridor);
+    setScheduleKoridor(new_schedule_koridor[0].koridor_schedule);
+    hideModal();
+  };
 
   return (
     <>
@@ -219,11 +96,11 @@ export function HomeScreen() {
                   fontWeight: "300",
                 }}
               >
-                Koridor Name: 1
+                Koridor Name: {koridorName}
               </Text>
               <Text style={{ fontWeight: "300" }}>13/07/2023 19:43</Text>
             </View>
-            <TouchableOpacity onPress={showModal}>
+            <TouchableOpacity onPress={() => onHandleFilterKoridor()}>
               <MaterialCommunityIcons
                 style={{ marginTop: hp("4%"), marginRight: wp("2%") }}
                 name="filter"
@@ -245,33 +122,24 @@ export function HomeScreen() {
                   <Text style={{ marginBottom: hp("2%") }}>Filter Koridor</Text>
                   <Divider />
                   <RadioButton.Group
-                    onValueChange={(newValue) => setValue(newValue)}
-                    value={value}
+                    onValueChange={(newValue) =>
+                      onChangeFilterKoridor(newValue)
+                    }
+                    value={valueKoridor}
                   >
-                    <View style={{ flexDirection: "row" }}>
-                      <RadioButton value="routes" />
-                      <Text style={{ marginTop: hp("1%") }}>Koridor 1</Text>
-                    </View>
-                    <View style={{ flexDirection: "row" }}>
-                      <RadioButton value="day" />
-                      <Text style={{ marginTop: hp("1%") }}>Koridor 2</Text>
-                    </View>
-                    <View style={{ flexDirection: "row" }}>
-                      <RadioButton value="day" />
-                      <Text style={{ marginTop: hp("1%") }}>Koridor 2A</Text>
-                    </View>
-                    <View style={{ flexDirection: "row" }}>
-                      <RadioButton value="a3" />
-                      <Text style={{ marginTop: hp("1%") }}>Koridor 3</Text>
-                    </View>
+                    {koridor_name.map((e) => (
+                      <View key={e.key} style={{ flexDirection: "row" }}>
+                        <RadioButton value={e.name} />
+                        <Text style={{ marginTop: hp("1%") }}>
+                          {"Koridor " + e.name}
+                        </Text>
+                      </View>
+                    ))}
                   </RadioButton.Group>
                   <Button
                     style={{ marginTop: hp("2%") }}
                     buttonColor="#FCC21B"
-                    onPress={() => {
-                      setValue("");
-                      hideModal();
-                    }}
+                    onPress={() => onOkFilterKoridor()}
                   >
                     <Text style={{ color: "white" }}>Ok</Text>
                   </Button>
@@ -282,116 +150,130 @@ export function HomeScreen() {
           <Divider />
           <ScrollView style={{ marginTop: hp("2%"), marginBottom: hp("10%") }}>
             {/* Table list of content */}
-            {dataJadwalBus.map((e) => (
-              <View
-                key={e.key}
-                style={{
-                  backgroundColor: "white",
-                  borderRadius: 10,
-                  shadowColor: "grey",
-                  marginBottom: hp("1%"),
-                }}
-              >
-                <List.Item
-                  title={"Halte: " + e.halte}
-                  description={
-                    <View style={{ flexDirection: "column" }}>
-                      <Text
-                        style={{
-                          fontSize: wp("3%"),
-                          fontWeight: "300",
-                        }}
-                      >
-                        date: {e.date}
-                      </Text>
-                      <Text
-                        style={{
-                          fontSize: wp("3%"),
-                          fontWeight: "300",
-                        }}
-                      >
-                        departure time: {e.deparature_time_in_koridor}
-                      </Text>
-                      <Text
-                        style={{
-                          fontSize: wp("3%"),
-                          fontWeight: "300",
-                        }}
-                      >
-                        arrival time: {e.arrival_time_in_koridor}
-                      </Text>
-                    </View>
-                  }
-                  left={(props) => (
-                    <List.Icon {...props} icon="bus" color="#FCC21B" />
-                  )}
-                  right={(props) => (
-                    <>
-                      <TouchableOpacity onPress={showDetail}>
-                        <List.Icon {...props} icon="dots-vertical" />
-                      </TouchableOpacity>
-                      <Portal>
-                        <Modal
-                          visible={detail}
-                          onDismiss={hideDetail}
-                          contentContainerStyle={{
-                            backgroundColor: "white",
-                            padding: wp("5%"),
-                            margin: wp("5%"),
-                          }}
-                        >
-                          <View
-                            style={{
-                              flexDirection: "row",
-                              justifyContent: "space-between",
-                              //marginBottom: hp("3%"),
-                            }}
-                          >
-                            <Text>Detail Halte</Text>
-                            <TouchableOpacity onPress={showModal}>
-                              <MaterialCommunityIcons
-                                style={{
-                                  marginRight: wp("2%"),
-                                }}
-                                name="filter"
-                                color="#FCC21B"
-                                size={26}
-                              />
-                            </TouchableOpacity>
-                          </View>
-                          <Divider
-                            style={{
-                              marginTop: hp("1%"),
-                              marginBottom: hp("3%"),
-                            }}
-                          />
-                          {e.schedule_koridor.map((e) => (
-                            <List.Item
-                              key={e.index}
-                              title={"Bus: " + e.bus_queue}
-                              description={
-                                <View style={{ flexDirection: "column" }}>
-                                  <Text>
-                                    {"departure time bus: " +
-                                      e.deparature_time_bus}
-                                  </Text>
-                                  <Text>
-                                    {"arrival time bus: " + e.arrival_time_bus}
-                                  </Text>
-                                </View>
-                              }
-                            />
-                          ))}
-                          <Button onPress={hideDetail}>
-                            <Text style={{ color: "#40C0E7" }}>Close</Text>
-                          </Button>
-                        </Modal>
-                      </Portal>
-                    </>
-                  )}
+
+            {scheduleKoridor.length === 0 ? (
+              <View style={{ alignItems: "center", marginTop: hp("10%") }}>
+                <Text>No Data</Text>
+                <MaterialCommunityIcons
+                  style={{ marginRight: wp("2%") }}
+                  name="folder-remove-outline"
+                  color="black"
+                  size={50}
                 />
               </View>
-            ))}
+            ) : (
+              scheduleKoridor.map((e: any) => (
+                <View
+                  key={e.key}
+                  style={{
+                    backgroundColor: "white",
+                    borderRadius: 10,
+                    shadowColor: "grey",
+                    marginBottom: hp("1%"),
+                  }}
+                >
+                  <List.Item
+                    title={"Halte: " + e.halte}
+                    description={
+                      <View style={{ flexDirection: "column" }}>
+                        <Text
+                          style={{
+                            fontSize: wp("3%"),
+                            fontWeight: "300",
+                          }}
+                        >
+                          date: {e.date}
+                        </Text>
+                        <Text
+                          style={{
+                            fontSize: wp("3%"),
+                            fontWeight: "300",
+                          }}
+                        >
+                          departure time: {e.deparature_time_in_koridor}
+                        </Text>
+                        <Text
+                          style={{
+                            fontSize: wp("3%"),
+                            fontWeight: "300",
+                          }}
+                        >
+                          arrival time: {e.arrival_time_in_koridor}
+                        </Text>
+                      </View>
+                    }
+                    left={(props) => (
+                      <List.Icon {...props} icon="bus" color="#FCC21B" />
+                    )}
+                    right={(props) => (
+                      <>
+                        <TouchableOpacity onPress={showDetail}>
+                          <List.Icon {...props} icon="dots-vertical" />
+                        </TouchableOpacity>
+                        <Portal>
+                          <Modal
+                            visible={detail}
+                            onDismiss={hideDetail}
+                            contentContainerStyle={{
+                              backgroundColor: "white",
+                              padding: wp("5%"),
+                              margin: wp("5%"),
+                            }}
+                          >
+                            <View
+                              style={{
+                                flexDirection: "row",
+                                justifyContent: "space-between",
+                                //marginBottom: hp("3%"),
+                              }}
+                            >
+                              <Text>Detail Halte</Text>
+                              <TouchableOpacity onPress={showModal}>
+                                <MaterialCommunityIcons
+                                  style={{
+                                    marginRight: wp("2%"),
+                                  }}
+                                  name="filter"
+                                  color="#FCC21B"
+                                  size={26}
+                                />
+                              </TouchableOpacity>
+                            </View>
+                            <Divider
+                              style={{
+                                marginTop: hp("1%"),
+                                marginBottom: hp("3%"),
+                              }}
+                            />
+                            {e.schedule_koridor.map((e: any) => (
+                              <List.Item
+                                key={e.index}
+                                title={"Bus: " + e.bus_queue}
+                                description={
+                                  <View style={{ flexDirection: "column" }}>
+                                    <Text>
+                                      {"departure time bus: " +
+                                        e.deparature_time_bus}
+                                    </Text>
+                                    <Text>
+                                      {"arrival time bus: " +
+                                        e.arrival_time_bus}
+                                    </Text>
+                                  </View>
+                                }
+                              />
+                            ))}
+                            <Button onPress={hideDetail}>
+                              <Text style={{ color: "#40C0E7" }}>Close</Text>
+                            </Button>
+                          </Modal>
+                        </Portal>
+                      </>
+                    )}
+                  />
+                </View>
+              ))
+            )}
           </ScrollView>
         </View>
       </PaperProvider>
